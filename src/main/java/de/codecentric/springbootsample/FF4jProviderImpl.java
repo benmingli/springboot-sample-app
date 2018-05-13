61bd2de6da084a5597bf041115b7fe22d6a1c08c
Ming Li <benmingli@gmail.com>
2018-05-13 00:04:33 -0700

    Make thread running (so every 5 seconds, NEW ff4j is created)

--- src/main/java/de/codecentric/springbootsample/FF4jProviderImpl.java
+++ src/main/java/de/codecentric/springbootsample/FF4jProviderImpl.java
@@ -1,5 +1,8 @@
 package de.codecentric.springbootsample;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by mingli on 12/05/18.
  */
@@ -7,20 +10,23 @@ public class FF4jProviderImpl implements FF4jProvider {
     private FF4j ff4j;
 
     public FF4jProviderImpl() {
-        new Thread(new Runnable() {
+        ExecutorService executor = Executors.newFixedThreadPool(1);
+        executor.execute(new Runnable() {
             @Override
             public void run() {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                ff4j = new FF4j("od");
-                if (null != listener) {
-                    listener.OnFF4jUpdated(ff4j);
+                while (true) {
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    ff4j = new FF4j("od");
+                    if (null != listener) {
+                        listener.OnFF4jUpdated(ff4j);
+                    }
                 }
             }
-        }).start();
+        });
     }
 
     @Override
